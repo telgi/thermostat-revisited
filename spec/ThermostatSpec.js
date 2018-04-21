c6f526989d55a670a9254adddd30835f1bb47224
Terry Allitt <tallitt88@gmail.com>
2018-04-21 11:49:13 +0100

    :sparkles: Temperature can be increased by 1

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,11 @@ describe("Thermostat", function() {
   it("should start with a temperature of 20 degrees", function() {
     expect(thermostat.temperature()).toEqual(20);
   });
+
+  describe("Increasing Temperature", function() {
+    it("should increase the current temperature by 1", function() {
+      thermostat.increase();
+      expect(thermostat.temperature()).toEqual(21);
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -5,3 +5,7 @@ function Thermostat() {
 Thermostat.prototype.temperature = function () {
   return this._temperature;
 };
+
+Thermostat.prototype.increase = function () {
+  this._temperature++;
+};
