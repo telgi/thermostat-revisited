4566fa7e45328e4621876e1e4d85e45bcab61719
Terry Allitt <tallitt88@gmail.com>
2018-04-26 15:56:52 +0100

    :construction: Add energy usage status to interface

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
-  $("#temp").text(thermostat.temperature);
-
   $("#temp-up").click(function() {
     thermostat.up();
     $("#temp").text(thermostat.temperature);
+    setEnergyUsage();
   });
 
   $("#temp-down").click(function() {
     thermostat.down();
     $("#temp").text(thermostat.temperature);
+    setEnergyUsage();
   });
 
   $("#power-mode").click(function() {
     thermostat.setPowerSavingMode();
+    $("#temp").text(thermostat.temperature);
     if (thermostat._powerSavingMode === false) {
       $("#power-mode").text("Off")
     } else {
       $("#power-mode").text("On")
     }
   })
+
+  function setEnergyUsage() {
+    thermostat.checkEnergyUsage()
+    $("#energy-usage").text(thermostat._energyUsage)
+  };
 });
